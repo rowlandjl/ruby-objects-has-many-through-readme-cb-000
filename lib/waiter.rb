3218f4f568f5aea93cdbc3611f48e1c1b52e6db0
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 02:43:17 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,5 +16,9 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end 
+  
+  def meals 
+    Meal.all.select {|meal| meal.waiter == self}
+  end 
 
 end
